8c81ca3633beb1d676622a898191b5cba481cb54
margosia003 <124198916+margosia003@users.noreply.github.com>
2023-02-22 14:01:36 -0500

    playpause music

--- app/src/main/java/com/example/serviceapp/MainActivity.java
+++ app/src/main/java/com/example/serviceapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.serviceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,8 +15,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void playMusic(View view) {
+        Intent playMusicIntent = new Intent(this, MusicPlayerService.class);
+
+        startService(playMusicIntent);
     }
 
     public void pauseMusic(View view) {
+        Intent pauseMusicIntent = new Intent(this, MusicPlayerService.class);
+
+        stopService(pauseMusicIntent);
     }
 }
